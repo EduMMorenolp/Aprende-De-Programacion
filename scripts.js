295a00181057557e16515572a8b76febc01303c2
Luis Rodriguez <luishi.rodriguez@gmail.com>
2023-08-10 20:54:26 -0300

    Sin cambios

--- scripts.js
+++ scripts.js
@@ -7,7 +7,8 @@ function cargarLeccion(event) {
   fetch(enlace) // Obtiene el contenido de "Leccion1.html"
     .then((response) => response.text())
     .then((html) => {
-      contenidoLeccion.innerHTML = html; // Coloca el contenido en el div
+      contenidoLeccion.in;
+      nerHTML = html; // Coloca el contenido en el div
     })
     .catch((error) => {
       console.error("Error al cargar la lección:", error);
