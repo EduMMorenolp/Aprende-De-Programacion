75379ddf5e08aa016df52397c654654ad17e8aaf
Edu Moreno <e.m.morenolp@gmail.com>
2023-08-15 01:13:53 -0300

    fix y mejoras

--- barraNavegacion.js
+++ barraNavegacion.js
@@ -59,3 +59,19 @@ const barraNavengacionCod = `
 
 const nav = document.getElementById(`barraNavegacion`);
 nav.innerHTML = barraNavengacionCod;
+
+function cargarLeccion(event) {
+  event.preventDefault(); // Evita el comportamiento predeterminado del enlace (no abrirá en una nueva pestaña)
+
+  const enlace = event.target.getAttribute("href"); // Obtiene la ruta del enlace
+  const contenidoLeccion = document.getElementById("contenido-leccion"); // Obtén el div donde se cargará el contenido
+
+  fetch(enlace) // Obtiene el contenido de "Leccion1.html"
+    .then((response) => response.text())
+    .then((html) => {
+      contenidoLeccion.innerHTML = html; // Coloca el contenido en el div
+    })
+    .catch((error) => {
+      console.error("Error al cargar la lección:", error);
+    });
+}
--- scripts.js
+++ scripts.js
@@ -14,3 +14,25 @@ function cargarLeccion(event) {
       console.error("Error al cargar la lección:", error);
     });
 }
+ // Obtener referencia al formulario de registro
+ var registroForm = document.querySelector('#registroModal form');
+ let clientes = [];
+ registroForm.addEventListener('submit', function(event) {
+   event.preventDefault(); // Evitar que el formulario se envíe
+
+   // Obtener valores de los campos del formulario
+   var nombre = document.querySelector('#nombre').value;
+   var email = document.querySelector('#email').value;
+   var contrasena = document.querySelector('#contrasena').value;
+
+   var cliente = {
+     nombre: nombre,
+     email: email,
+     contrasena: contrasena
+   };
+   clientes.push(cliente);
+   
+   // Mostrar el cliente en la consola
+   console.log('Nuevo cliente registrado:', cliente);
+   console.log(clientes)
+ });
